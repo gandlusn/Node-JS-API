e501b31852d6e9a62e986c314df0d6bc39f49003
gandlusn <saikishanuc@gmail.com>
2019-04-11 08:27:31 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const winston = require('winston');
+const express = require('express');
+const app = express();
+
+require('./startup/logging')();
+require('./startup/routes')(app);
+require('./startup/db')();
+require('./startup/config')();
+require('./startup/validation')();
+
+//run this command
+//$env:vidly_jwtPrivateKey=”password”
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
